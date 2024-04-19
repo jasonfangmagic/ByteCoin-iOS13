e53947589fc6de14095e3da04ec592355f8239a7
jasonfangmagic <jasonfangmagic@gmail.com>
2024-04-19 11:18:25 -0400

    add api key

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -10,10 +10,13 @@ import Foundation
 
 struct CoinManager {
     
+    
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "EBD2EFF3-9992-4097-9AAF-B1D268A11624"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     
+    func getCoinPrice(for currency: String)  {
+    }
 }
